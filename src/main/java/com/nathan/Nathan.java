f4498366d58b96286eae281954aae8bb9b6674ab
Benjamin GROISNE <125885157+BenjaminGROISNE@users.noreply.github.com>
2026-06-30 01:48:41 +0200

    Publish Nathan 1.0.0 from BotMaker Studio

---
+++ src/main/java/com/nathan/Nathan.java
@@ -0,0 +1,11 @@
+package com.nathan;
+import com.botmaker.sdk.api.BotMaker;
+
+public class Nathan {
+    public static void main(String[] args) {
+        BotMaker.print("Hello from Nathan!");
+		int number = 26;
+		double decimal = number;
+		BotMaker.print(decimal);
+    }
+}
